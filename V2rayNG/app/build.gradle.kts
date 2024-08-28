335a8a4beca7de6bd7edf61652bb03804dae6a75
Eken Chan <maskedeken@yahoo.com>
2024-08-28 12:16:03 +0800

    fix apk release

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -20,6 +20,7 @@ android {
         splits {
             abi {
                 isEnable = true
+                reset()
                 include(
                     "arm64-v8a",
                     "armeabi-v7a",
